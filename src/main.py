c1dcf34894d0ad92ef5d7957e9765b2c9ebae9f0
Alexandar Kraunsøe <alexandar_slambert@hotmail.com>
2026-02-20 10:09:18 +0100

    Safety check
    Updated main, to check sys.argv for 2 indices

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import sys
 from generator import move_files, generate_pages_recursive
 
 def main():
-    basepath = sys.argv[1] or "/"
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
     move_files("static", "docs")
     generate_pages_recursive("content", "template.html", "docs", basepath)
 
